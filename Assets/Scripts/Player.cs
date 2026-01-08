51e3943a29a345404dddb2c8e933deee545e2dc2
flare-seyn <155430545+flare-seyn@users.noreply.github.com>
2026-01-07 22:54:24 -0800

    Match seed count UI labels

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,49 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        if (_playerTransform == null)
+        {
+            _playerTransform = transform;
+        }
+
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        UpdateSeedUI();
     }
 
     private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * (_speed * Time.deltaTime);
+        _playerTransform.Translate(movement, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft <= 0 || _plantPrefab == null)
+        {
+            return;
+        }
+
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+        UpdateSeedUI();
+    }
+
+    private void UpdateSeedUI()
+    {
+        if (_plantCountUI == null)
+        {
+            return;
+        }
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
